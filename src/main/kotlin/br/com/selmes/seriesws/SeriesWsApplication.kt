3b86458f7738274c0daac304eaf77cabf964c8ec
Leonardo Rodrigues <leo_rse@hotmail.com>
2022-01-06 17:23:39 -0300

    Project Created

---
+++ src/main/kotlin/br/com/selmes/seriesws/SeriesWsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.selmes.seriesws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeriesWsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeriesWsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/selmes/seriesws/SeriesWsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.selmes.seriesws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeriesWsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
