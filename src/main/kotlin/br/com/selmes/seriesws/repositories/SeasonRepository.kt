daeab0e26aae1c455c42fc235a468227259059aa
Leonardo Rodrigues <leo_rse@hotmail.com>
2022-01-07 15:11:24 -0300

    feat(Repositories) add repository anotation

--- src/main/kotlin/br/com/selmes/seriesws/repositories/SeasonRepository.kt
+++ src/main/kotlin/br/com/selmes/seriesws/repositories/SeasonRepository.kt
@@ -2,6 +2,8 @@ package br.com.selmes.seriesws.repositories
 
 import br.com.selmes.seriesws.models.Season
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SeasonRepository: JpaRepository<Season, Long> {
 }
--- src/main/kotlin/br/com/selmes/seriesws/repositories/SerieRepository.kt
+++ src/main/kotlin/br/com/selmes/seriesws/repositories/SerieRepository.kt
@@ -2,6 +2,8 @@ package br.com.selmes.seriesws.repositories
 
 import br.com.selmes.seriesws.models.Serie
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SerieRepository: JpaRepository<Serie, Long> {
 }
